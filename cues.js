9d0bf5ce1b6dc00463e35d7d985831a37c5f8a51
Simon Emmanuel Roux <simonshop@studioimaginaire.com>
2011-08-16 10:35:56 -0400

    More content
    - More content  - Moved some of .js libs - Visual timeline with standard
    player (only works in Chrome)

--- cues.js
+++ cues.js
@@ -1,18 +1,112 @@
-  // When the document is loaded...
+// When the document is loaded...
   $(document).ready(function() {
      // Create a popcorn instance by calling Popcorn("#id-of-my-video")
      var pop = Popcorn("#gdm")
-
-  	// Scroll the page
+     
+     
+// The scrolling actions underlines de different sections of the show, the other scripts happen when a section is loaded.
+ 
+// Section 1 
+  	pop.scrolling ({
+  	 start: 0, 
+  	  end: 48,
+  	  target: 'section1'
+  	  })
+  	  
+  	  
+// Section 2
   	.scrolling({
-  	  start: 4, 
-  	  end: 45,
+  	  start: 49, 
+  	  end: 197,
   	  target: 'section2'
   	})
   	
+ // Section 3 
     .scrolling({
-      start: 197,
-      end: 198,
+      start: 198,
+      end: 418,
       target: 'section3'
     })
+
+ // Section 4 
+      .scrolling({
+      start: 419,
+      end: 573,
+      target: 'section4'
+    })
+
+  
+ // Section 5 
+      .scrolling({
+      start: 574,
+      end: 1072,
+      target: 'section5'
+    })
+
+  
+  
+ // Section 6 
+      .scrolling({
+      start: 1073,
+      end: 1188,
+      target: 'section6'
+    })
+
+  
+// Section 7
+  	.scrolling({
+  	  start: 1189, 
+  	  end: 1575,
+  	  target: 'section7'
+  	})
+  	
+ // Section 8 
+    .scrolling({
+      start: 1576,
+      end: 1637,
+      target: 'section8'
+    })
+
+ // Section 9 
+      .scrolling({
+      start: 1638,
+      end: 2214,
+      target: 'section9'
+    })
+
+  
+ // Section 10 
+      .scrolling({
+      start: 2215,
+      end: 2385,
+      target: 'section10'
+    })
+
+  
+ // Section 11 
+      .scrolling({
+      start: 2386,
+      end: 3036,
+      target: 'section11'
+    })
+
+  
+   
+ // Section 12 
+      .scrolling({
+      start: 3037,
+      end: 3114,
+      target: 'section12'
+    })
+
+  
+ // Section 13 
+      .scrolling({
+      start: 3115,
+      end: 3209,
+      target: 'section13'
+    })
   });
+  
+  
+  
